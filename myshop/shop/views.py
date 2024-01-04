4ce7c7edff8baa7216239c65feca84bce2176e4b
albertNSBM <albertnsaba2018@gmail.com>
2024-01-04 15:27:20 +0200

    adding the urls for our views

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -22,4 +22,11 @@ def product_list(request,category_slug=None):
 
 # view to display single product from the database
 
-def 
+def product_detail(request,id,slug):
+    product=get_object_or_404(Product,
+                              id=id,
+                              slug=slug,
+                              available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product':product})
