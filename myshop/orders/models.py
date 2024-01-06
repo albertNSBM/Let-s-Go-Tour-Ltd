b913b8e0547c2272684e1827c844ed4baebf1714
albertNSBM <albertnsaba2018@gmail.com>
2024-01-06 19:41:45 +0200

    finishing the ordering

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -29,13 +29,13 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order=models.ForeignKey(Order,
                             related_name='items',
-                            on_delete=models.CASCADE)
+                            on_delete=models.CASCADE,null=True)
     product=models.ForeignKey(Product,
                               related_name='order_items',
-                              on_delete=models.CASCADE)
+                              on_delete=models.CASCADE,null=True)
     price=models.DecimalField(max_digits=10,
-                              decimal_places=2)
-    quantity=models.PositiveIntegerField(default=1)
+                              decimal_places=2,null=True)
+    quantity=models.PositiveIntegerField(default=1,null=True)
     
     def __str__(self):
         return str(self.id)
