872400e097841f7638778c9ef5ad8823cb362808
albertNSBM <albertnsaba2018@gmail.com>
2024-01-10 16:54:30 +0200

    continuing to the payment

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -7,4 +7,50 @@ from orders.models import Order
 
 
 # Create the stripe instance 
+stipe.api_key=settings.STRIPE_SECRET_KEY
+stipe.api_version =settings.STRIPE_API_VERSION
 
+def payment_process(request):
+    order_id=request.session.get('order_id',None)
+    order=get_object_or_404(Order, id=order_id)
+    
+    if request.method=='POST':
+        success_url=request.build_absolute_uri(
+            reverse('payment:completed'))
+        
+        cancel_url=request.build_absolute_uri(
+            reverse('payment:canceled'))
+        
+        #stipe checkout session data
+        
+        session_data={
+            'mode':'payment',
+            'client_reference_id':order.id,
+            'success_url':success_url,
+            'cancel_url':cancel_url,
+            'line_items':[]
+        }
+        # add order item to the stipe checkout session
+        for item in order.items.all():
+            session_data['line_items'].append({
+                'price_data':{
+                    'unit_amout':int(item.price *Decimal('100')),
+                    'currency':'usd',
+                    'product_data':{
+                        'name':item.product.name,
+                    },
+                },
+                'quantity':item.quantity,
+            })
+        # creating stripe check out session
+        session=stripe.checkout.Session.create(**session_data)
+        #redirect to Stripe payment form
+        return redirect(session.url, code=303)
+    else:
+        return render(request, 'payment/process.html',locals())
+
+def payment_completed(request):
+    return render(request, 'payment/completed.html')
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
